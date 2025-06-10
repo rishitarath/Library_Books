d015eb71e3233d981a041404434bbe0564a821b9
Rishita Rath <rr3011@hw.ac.uk>
2025-06-10 10:25:55 +0100

    Create GraphicNovel.java

---
+++ GraphicNovel.java
@@ -0,0 +1,7 @@
+package bookdb;
+
+public interface GraphicNovel {
+	public boolean getPalette(); // Returns Full Colour if color, black and white if black-and-white
+    public int getPages(); // Returns the number of pages
+
+}
